64fb089f1b0825722d2d5a2f073e830b7cd692b9
khalid53 <khalid.metavizoffice@gmail.com>
2024-11-16 16:55:14 +0500

    add the current login user updated  functionality

--- controllers/newUserControllers.js
+++ controllers/newUserControllers.js
@@ -1,5 +1,13 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const appError = require("../utils/appError");
+const filterObj = (obj, ...allowFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 exports.addNewUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -18,4 +26,22 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     status: "success",
     data: users,
   });
+});
+
+//this controller is used to update the current user
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new appError(
+        "this route is not for password update please use the route /updatePassword to change the password",
+        400
+      )
+    );
+  }
+  const filteredBody = filterObj(req.body, "name", "email");
+  const updatedUer = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ message: "success", updatedUer });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.patch("/reset-password/:token", authController.resetPassword);
+router.patch("/updateMe", authController.protect, userController.updateMe);
 router.patch(
   "/updateMyPassword",
   authController.protect,
